751f69957810b410d4836a93ea1d6778b2563c53
Raghvendra Singh Shekhawat <163868970+raghvendra-singh9@users.noreply.github.com>
2025-01-08 22:53:34 +0530

    Add files via upload

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -179,26 +179,78 @@ console.log(friends.includes(23));
 
 // in objects order of values doent matter when we retrieve
 //object literal syntax
-// const jonas = {
-//   firstName: 'Raghvendra',
-//   lastName: 'Singh',
-//   age: 2037 - 1991,
-//   job: 'teacher',
-//   friends: ['M', 'P', 'S'],
-// };
+const jonas = {
+  firstName: 'Raghvendra',
+  lastName: 'Singh',
+  birthYear: 1991,
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['M', 'P', 'S'],
+  hasDriversLicense: false,
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // }, we can do this also
 
-// console.log(jonas);
-// console.log(jonas.lastName);
-// console.log(jonas['lastName']);
+  // calcAge: function () {
+  //   console.log(this);
+  //   return 2037 - this.birthYear;
+  // },  we can do this also
 
-// const nameKey = 'Name';
-// console.log(jonas['first' + nameKey]);
-// console.log(jonas['last' + nameKey]);
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
 
-// const interestedIn = prompt(
-//   'What do you want to know about Raghvendra? Choose between firstName, lastName, age, job, and friends'
-// );
-// console.log(jonas[interestedIn]);
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}- year old ${
+      this.job
+    }, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`;
+  },
+};
 
-// jonas.location = 'Portugal';
-// jonas['twitter'] = '@raghvendra';
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt(
+  'What do you want to know about Raghvendra? Choose between firstName, lastName, age, job, and friends'
+);
+console.log(jonas[interestedIn]);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose between firstName, lastName, age, job, and friends'"
+  );
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@raghvendra';
+console.log(jonas);
+
+//CHALLENGE
+//"Jonas has 3 friends, and his best friend is called Michael"
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`
+);
+
+// OBJECT METHODS
+
+console.log(jonas.calcAge());
+console.log(jonas.calcAge());
+console.log(jonas.calcAge());
+console.log(jonas['calcAge'](1991));
+
+console.log(jonas.age);
+
+// CHALLENGE
+// "Jonas is a 46-year old teacher. and he has a/no driver's license"
+
+console.log(jonas.getSummary());
+
+//LOOPS
