1a781a6dd712026861dce72fb2ecbef04d9826d0
Raghvendra Singh Shekhawat <163868970+raghvendra-singh9@users.noreply.github.com>
2025-01-28 22:58:54 +0530

    Add files via upload

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,18 @@ const restaurant = {
     },
   },
 };
+
+// Array destructuring
+
+const arr = [2, 3, 4];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, second] = restaurant.categories;
+console.log(first, second);
